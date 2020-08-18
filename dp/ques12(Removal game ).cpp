617bc66bacea4b4d0d2846b663911b1a0d3f74c2
Ankit Kumar <66835391+ankit-1S@users.noreply.github.com>
2020-08-18 17:08:15 +0530

    Update ques12(Removal game ).cpp

--- dp/ques12(Removal game ).cpp
+++ dp/ques12(Removal game ).cpp
@@ -1,31 +1,45 @@
-// problem set link -- https://cses.fi/problemset/  
-
 /* UserName - ankit_S
-  author- Ankit kumar
+  author -Ankit kumar
 */
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 #define FAST ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-#define ll unsigned long long 
-#define MOD 1000000007
-vector<ll int>arr(5001);
-int fun(int low,int high){
-    if(low==high){
-        return arr[low];
-    }
-    if(low>high){
-        return 0;
-    }
-    return max( arr[low]+min( fun(low+2,high),fun(low+1,high-1) ) , arr[high]+min(fun(low+1,high-1),fun(low,high-2)));
-}
+#define ll long long 
+const ll int MOD=1e9+7;
 
-int main(){
-    int n;
-    cin>>n;
+int main()
+{
+    FAST
+    ll int n;
+    cin>> n;
+    vector<ll int> arr(n);
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+    	cin>> arr[i];
+   	}
+    ll int dp[n][n][2];
+    ll int ans=0;
+    for(int len=1;len<=n;len++){
+        for(int i=0;i<n;i++){
+            int j=len+i-1;
+            if(j>=i && j<n){
+                if(i==j){
+                    dp[i][j][0]=arr[i];
+                    dp[i][j][1]=0;
+                }
+                else{
+                    ll int x=arr[i]+dp[i+1][j][1];
+                    ll int y=arr[j]+dp[i][j-1][1];
+                    //cout<<x<<" "<< y<<" "<<len<<i<<" "<<j<<" "<< endl;
+                    dp[i][j][0]=max(x,y);
+                    ans=max(ans,dp[i][j][0]);
+                    if(dp[i][j][0]==x)dp[i][j][1]=dp[i+1][j][0];
+                    else dp[i][j][1]=dp[i][j-1][0];
+                }
+            }
+        }
     }
-    cout<<fun(0,n-1);
-
+    cout<<dp[0][n-1][0] <<endl;
+    return 0;
 }
